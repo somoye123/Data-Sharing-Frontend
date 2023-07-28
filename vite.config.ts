2b7a213f6be6cf793294d6645a677853a7faa87d
somoye123 <somoye.ayotunde@gmail.com>
2023-07-28 16:48:19 +0100

    configure loading of environmental variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), EnvironmentPlugin('all')],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+    fs: {
+      strict: true,
+    },
+  },
+});
