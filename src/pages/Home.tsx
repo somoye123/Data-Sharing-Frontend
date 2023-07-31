1d2b4d406a91ae7bd32794433a40f89ac24982bf
somoye123 <somoye.ayotunde@gmail.com>
2023-07-31 09:20:53 +0100

    add styling for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../hooks';
 import { updateUser } from '../redux/userSlice';
 import { useUser } from '../hooks';
+import { toast } from 'react-toastify';
+import styled from 'styled-components';
 
 /**
  * Home component.
@@ -24,11 +26,11 @@ const Home = (): JSX.Element => {
   }
 
   const [state, setState] = useState<FormData>({
-    companyName: '',
-    numUsers: 0,
-    numProducts: 0,
-    percentage: 0,
-    logo: null,
+    companyName: user?.companyDetails?.companyName || '',
+    numUsers: user?.companyDetails?.numUsers || 0,
+    numProducts: user?.companyDetails?.numProducts || 0,
+    percentage: user?.companyDetails?.percentage || 0,
+    logo: user?.companyDetails?.logo || null,
   });
 
   const [isEdit, setIsEdit] = useState<boolean>(false);
@@ -36,13 +38,15 @@ const Home = (): JSX.Element => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     try {
       event.preventDefault();
-      const percentage: number = (state.numUsers / state.numProducts) * 100;
+      if (state?.companyName === '')
+        return toast.error('Company name is required');
       dispatch(
         updateUser({
+          ...user?.companyDetails,
           companyName: state?.companyName,
           numUsers: state?.numUsers,
           numProducts: state?.numProducts,
-          percentage: Number.isNaN(percentage) ? 0 : percentage,
+          percentage: state?.percentage,
         })
       );
       setIsEdit(false);
@@ -57,7 +61,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   useEffect(() => {
-    if (user && user?.companyDetails) {
+    if (user && user?.companyDetails && isEdit === false) {
       setState({
         companyName: user?.companyDetails?.companyName || '',
         numUsers: user?.companyDetails?.numUsers || 0,
@@ -71,28 +75,26 @@ const Home = (): JSX.Element => {
   if (status === 'loading') return <h2>Saving to Database</h2>;
 
   return (
-    <>
-      <h1>Welcome to the Data Sharing Application!</h1>
+    <Wrapper>
+      <Header>Welcome to the Data Sharing Application!</Header>
       {user && user?.companyDetails && isEdit === false ? (
-        <>
-          <h1>Company Name: {state?.companyName}</h1>
-          <h1>Number of Users: {state?.numUsers}</h1>
-          <h1>Number of Products: {state?.numProducts}</h1>
-          <h1>Percentage: {state?.percentage}</h1>
-          {state?.logo && (
-            <div>
-              <img src={state?.logo} alt="company logo" />
-            </div>
-          )}
-          <button type="button" onClick={() => setIsEdit(true)}>
-            Edit Company Details
-          </button>
-        </>
+        <CompanyDetails>
+          <h3>Company Name: {state?.companyName}</h3>
+          <h3>Number of Users: {state?.numUsers}</h3>
+          <h3>Number of Products: {state?.numProducts}</h3>
+          <h3>Percentage: {state?.percentage}%</h3>
+          {state?.logo && <Image src={state?.logo} alt="company logo" />}
+          <div>
+            <Button type="button" onClick={() => setIsEdit(true)}>
+              Edit Company Details
+            </Button>
+          </div>
+        </CompanyDetails>
       ) : (
         <form onSubmit={handleSubmit}>
-          <h2>Fill the form below</h2>
-          <label htmlFor="companyName">Company Name:</label>
-          <input
+          <Header>Fill the form below</Header>
+          <Label htmlFor="companyName">Company Name:</Label>
+          <Input
             type="text"
             id="companyName"
             value={state.companyName}
@@ -100,28 +102,94 @@ const Home = (): JSX.Element => {
               setState({ ...state, companyName })
             }
           />
-          <label htmlFor="numUsers">Number of Users:</label>
-          <input
+          <Label htmlFor="numProducts">Number of Products:</Label>
+          <Input
+            type="number"
+            id="numProducts"
+            value={state.numProducts}
+            onChange={({ target: { value } }) =>
+              setState({
+                ...state,
+                numProducts: parseInt(value),
+                percentage: Math.ceil((state.numUsers / parseInt(value)) * 100),
+              })
+            }
+          />
+          <Label htmlFor="numUsers">Number of Users:</Label>
+          <Input
             type="number"
             id="numUsers"
             value={state.numUsers}
             onChange={({ target: { value } }) =>
-              setState({ ...state, numUsers: parseInt(value) })
+              setState({
+                ...state,
+                numUsers: parseInt(value),
+                percentage: Math.ceil(
+                  (parseInt(value) / state.numProducts) * 100
+                ),
+              })
             }
           />
-          <label htmlFor="numProducts">Number of Products:</label>
-          <input
+          <Label htmlFor="percentage">Percentage:</Label>
+          <Input
             type="number"
-            value={state.numProducts}
-            onChange={({ target: { value } }) =>
-              setState({ ...state, numProducts: parseInt(value) })
-            }
+            id="percentage"
+            value={state.percentage}
+            readOnly
           />
-          <button type="submit">Submit To Database</button>
+          <Button type="submit">Submit</Button>
         </form>
       )}
-    </>
+    </Wrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Header = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const CompanyDetails = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
+
+const Image = styled.img`
+  max-width: 200px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  margin-top: 1rem;
+  font-size: 18px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
