1e4c2a751a277dbf8335d83dd6aba93bb7f639ba
somoye123 <somoye.ayotunde@gmail.com>
2023-07-31 09:32:20 +0100

    add styling for not found and unathorized pages

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,48 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-  return <div>Not found</div>;
+  return (
+    <NotFoundContainer>
+      <NotFoundTitle>404</NotFoundTitle>
+      <NotFoundText>Page Not Found</NotFoundText>
+      <Link to="/">
+        <BackButton>Go Back to Home</BackButton>
+      </Link>
+    </NotFoundContainer>
+  );
 };
 
 export default NotFound;
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+`;
+
+const NotFoundTitle = styled.h1`
+  font-size: 80px;
+  font-weight: bold;
+  color: #ff0000;
+  margin-bottom: 10px;
+`;
+
+const NotFoundText = styled.p`
+  font-size: 24px;
+  color: #333;
+`;
+
+const BackButton = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+`;
--- src/pages/Unauthorized.tsx
+++ src/pages/Unauthorized.tsx
@@ -1,5 +1,50 @@
-const Unauthorized = () => {
-  return <div>Unauthorized</div>;
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const NotAuthorizedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+`;
+
+const NotAuthorizedTitle = styled.h1`
+  font-size: 80px;
+  font-weight: bold;
+  color: #ff0000;
+  margin-bottom: 10px;
+`;
+
+const NotAuthorizedText = styled.p`
+  font-size: 24px;
+  color: #333;
+`;
+
+const BackButton = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+`;
+
+const NotAuthorized = () => {
+  return (
+    <NotAuthorizedContainer>
+      <NotAuthorizedTitle>Not Authorized</NotAuthorizedTitle>
+      <NotAuthorizedText>
+        You don't have permission to access this page.
+      </NotAuthorizedText>
+      <Link to="/">
+        <BackButton>Go Back to Home</BackButton>
+      </Link>
+    </NotAuthorizedContainer>
+  );
 };
 
-export default Unauthorized;
+export default NotAuthorized;
